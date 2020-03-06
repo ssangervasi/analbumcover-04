d6dc71d5da10086cacddf2627e215de1e22190fa
manolo <manolosavi@me.com>
2020-03-05 23:43:26 -0800

    fuck it also commiting the swift version

---
+++ run code.playground/Contents.swift
@@ -0,0 +1,88 @@
+import UIKit
+
+struct Spelling {
+	func isCorrect(_ str: String) -> Bool {
+		
+		print(str)
+		
+		return ["the", "quick", "bro", "brown", "fox", "jumped", "over", "the", "lazy", "dog"].contains(str)
+	}
+}
+
+
+func rephrase(phrase: String?, spelling: Spelling, minWordLength: Int) -> String? {
+	if phrase == nil {
+		return nil
+	}
+	
+	var rephrasing = phrase!
+//		.replace(/[0-9!@#$%^&*()_+-=\';:",.></?\\|`~" ]/g, "")
+	
+	if rephrasing.count == 0 {
+		if minWordLength < 0 {
+			assertionFailure("max call stack")
+		}
+		
+		return nil
+		
+	} else if rephrasing.count == 1 {
+		if rephrasing == "A" {
+			return "a"
+		}
+		
+		return nil
+	}
+	
+	
+	var result: String?
+	var nextWord = findWord(rephrasing, spelling, minWordLength)
+	if nextWord != nil {
+		result = ""
+	}
+	while (nextWord != nil) {
+		result! += nextWord!
+		
+		let newLength = rephrasing.count - (nextWord!.replacingOccurrences(of: " ", with: "")).count
+		rephrasing = String(rephrasing.suffix(newLength))
+		nextWord = findWord(rephrasing, spelling, minWordLength)
+		
+		if (nextWord == nil) {
+			return result
+		}
+		
+		result! += " "
+	}
+	
+	return result
+}
+
+func findWord(_ phrase: String, _ spelling: Spelling, _ minWordLength: Int) -> String? {
+	if phrase.count < minWordLength {
+		return nil
+	}
+	
+	for i in minWordLength...phrase.count {
+		let potentialWord = String(phrase.prefix(i))
+		let spellingResult = spelling.isCorrect(potentialWord.lowercased())
+
+		if potentialWord.count >= minWordLength {
+			if spellingResult {
+				return potentialWord.lowercased()
+			}
+		}
+	}
+
+	return nil
+}
+
+
+
+
+
+print("\"" + (rephrase(phrase: "Thequickbrownfoxjumpedoverthelazydog", spelling: Spelling(), minWordLength: 1) ?? "-1") + "\"")
+
+
+
+
+
+
